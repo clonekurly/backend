9b0010fa3544ec4b3d4dd2c62eb3898d320a15cf
offsujin <offsujin@gmail.com>
2022-01-20 14:44:33 +0900

    Initial commit

---
+++ src/main/java/com/marketkurly/clonekurly/ClonekurlyApplication.java
@@ -0,0 +1,13 @@
+package com.marketkurly.clonekurly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonekurlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClonekurlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketkurly/clonekurly/ClonekurlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketkurly.clonekurly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonekurlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
