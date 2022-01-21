cc4a35da73c8afaa92c56c266fdf80afc7760282
offsujin <offsujin@gmail.com>
2022-01-21 23:52:03 +0900

    feat: Add Classes related to Login

---
+++ src/main/java/com/marketkurly/clonekurly/sevice/MemberService.java
@@ -0,0 +1,20 @@
+package com.marketkurly.clonekurly.sevice;
+
+import com.marketkurly.clonekurly.domain.Member;
+import com.marketkurly.clonekurly.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Member getMemberInfo(String email) {
+        return memberRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("유저 정보가 없습니다."));
+    }
+}
