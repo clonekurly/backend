bf526273b1b518d3513d78f2ecc6f1db37fb0c78
offsujin <offsujin@gmail.com>
2022-01-27 13:18:40 +0900

    feat: "Auth 관련 로직 추가"

---
+++ src/main/java/com/marketkurly/clonekurly/sevice/AuthService.java
@@ -0,0 +1,47 @@
+package com.marketkurly.clonekurly.sevice;
+
+import com.marketkurly.clonekurly.controller.dto.MemberRequestDto;
+import com.marketkurly.clonekurly.controller.dto.MemberResponseDto;
+import com.marketkurly.clonekurly.controller.dto.TokenDto;
+import com.marketkurly.clonekurly.domain.Member;
+import com.marketkurly.clonekurly.repository.MemberRepository;
+import com.marketkurly.clonekurly.security.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
+
+    @Transactional
+    public MemberResponseDto signup(MemberRequestDto memberRequestDto) {
+        if (memberRepository.existsByEmail(memberRequestDto.getEmail())) {
+            throw new RuntimeException("이미 가입되어 있는 유저입니다.");
+        }
+
+        Member member = memberRequestDto.toMember(passwordEncoder);
+
+        return MemberResponseDto.of(memberRepository.save(member));
+    }
+
+    @Transactional
+    public TokenDto login(MemberRequestDto memberRequestDto) {
+        UsernamePasswordAuthenticationToken authenticationToken = memberRequestDto.toAuthentication();
+
+        // 사용자 비밀번호 검증
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+
+        return tokenDto;
+    }
+}
