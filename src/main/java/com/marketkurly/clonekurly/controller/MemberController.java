12ab0eb8a286923ec0e8bebb785bc29b220650f1
offsujin <offsujin@gmail.com>
2022-01-27 13:19:04 +0900

    feat: "Auth 관련 class 추가"

--- src/main/java/com/marketkurly/clonekurly/controller/MemberController.java
+++ src/main/java/com/marketkurly/clonekurly/controller/MemberController.java
@@ -1,16 +1,15 @@
 package com.marketkurly.clonekurly.controller;
 
 import com.marketkurly.clonekurly.controller.dto.MemberResponseDto;
-import com.marketkurly.clonekurly.domain.Member;
 import com.marketkurly.clonekurly.sevice.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
