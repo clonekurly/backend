704485645cf6e53e3fd34b8d8321406348862188
Joy_Lee <rottenpear99@naver.com>
2022-02-03 23:28:38 +0900

    Create member Entity

---
+++ src/main/java/com/marketkurly/clonekurly/domain/member/Members.java
@@ -0,0 +1,64 @@
+package com.marketkurly.clonekurly.domain.member;
+
+//실제 member 테이블과 매칭될 클래스,entity class 
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.crypto.Data;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "members")
+
+
+public class Members {
+
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     @Column(name="id",nullable = false) 
+     private Long id;
+     
+     @Column(length = 225, name = "password",nullable = false)
+     private String password;
+
+     @Column(length = 45, name = "name",nullable = false)
+     private String name;
+
+     @Column(length = 100, name="email",nullable = false)
+     private String email;
+
+     @Column(length = 20,name="phone",nullable = false)
+     private String phone;
+
+     @Column(length = 45,name="grade")
+     private String grade;
+
+     @Column(length = 400, name = "address",nullable = false)
+     private String address;
+
+     @Column(name="brithdate")
+     private Date birthDate;
+
+     @Column(length = 2, name = "gender")
+     private String gender;
+
+     @Temporal(TemporalType.TIMESTAMP)
+     private Date joineDate;
+
+}
